6a5f8a84af8fbaee0b3e3fc0d83b4f16fbcabd14
Shivank Dixit <shivankdixit@Shivanks-MacBook-Air.local>
2025-06-26 14:10:01 +0530

    pushed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -93,50 +93,7 @@ export default function Navbar() {
              <div className='px-4 py-2 bg-green-500 hover:bg-green-300 rounded cursor-pointer'><SignUpButton>Login</SignUpButton></div> : 
              <div className='px-4 py-2 bg-red-500 hover:bg-red-300 rounded cursor-pointer'><SignOutButton>Logout</SignOutButton></div>
             } 
-
-            {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
-              <div>
-                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    className="size-8 rounded-full"
-                  />
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-              >
-                <MenuItem>
-                  <Link
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Your Profile
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Settings
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    <SignOutButton> Sign out </SignOutButton>
-                  </Link>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
+ 
           </div>
         </div>
       </div>
